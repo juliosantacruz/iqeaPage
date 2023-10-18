458eca0547a30443c9b9ebf90da9c65b57b9c099
Julio Santacruz <julio.santacruzh@gmail.com>
2023-10-18 11:48:37 -0700

    Styles were added for the desktop size of the navbar section

--- src/layout/Navbar/index.tsx
+++ src/layout/Navbar/index.tsx
@@ -1,18 +1,57 @@
+'use client'
+
 import React from "react";
 import "./Navbar.scss";
 import Link from "next/link";
+import {  usePathname } from 'next/navigation';
+import Image from "next/image";
+import logoIqea from "@/assets/iqea_logo.png";
 
 export default function Navbar() {
+  const path =  usePathname() 
+
+const isActiveLink = (value:string) =>{
+  return `navLink ${value===path?'active':''}`
+}
+
   return (
     <header>
-      <nav>
-        <Link href={'#'}>Inicio</Link>
-        <Link href={'#'}>Quienes Somos</Link>
-        <Link href={'#'}>Servicios</Link>
-        <Link href={'#'}>Productos</Link>
-
-        <Link href={'#'}>Proyectos</Link>
-        <Link href={'#'}>Contacto</Link>
+      <nav className="navbar">
+        <div className="iqeaLogo">
+          <Image src={logoIqea} alt="logo" className="logoIqea" />
+        </div>
+        <ul className="navLinks">
+          <li>
+            <Link href="/" className={isActiveLink('/')}>
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link href='/quienes-somos' className={isActiveLink('/quienes-somos')}>
+              Quiénes Somos
+            </Link>
+          </li>
+          <li>
+            <Link href="/servicios" className={isActiveLink('/servicios')}>
+              Servicios
+            </Link>
+          </li>
+          <li>
+            <Link href='/productos' className={isActiveLink('/productos')}>
+              Productos
+            </Link>
+          </li>
+          <li>
+            <Link href="/proyectos" className={isActiveLink('/proyectos')}>
+              Proyectos
+            </Link>
+          </li>
+          <li>
+            <Link href="/contacto" className={isActiveLink('/contacto')}>
+              Contacto
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
