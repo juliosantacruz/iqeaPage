5c2c51655f1bbda36a5e28c49cbe2fee5ceecab5
Julio Santacruz <julio.santacruzh@gmail.com>
2023-10-20 11:26:51 -0700

    creacion de seccion productos y cardDummy

---
+++ src/components/Icons/IconArrowRight.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+type Props={className?:string}
+export default function IconArrowRight({className}:Props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className={className}><path d="M10.061 19.061 17.121 12l-7.06-7.061-2.122 2.122L12.879 12l-4.94 4.939z"></path></svg>
+  )
+}
--- src/sections/Products/index.tsx
+++ src/sections/Products/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import './Products.scss'
+import React from "react";
+import "./Products.scss";
+import ProductCard from "@/components/ProductCard";
 
 export default function Products() {
   return (
-    <section className='productsIqea'>soy products</section>
-  )
+    <section className="productsIqea">
+      <div className="productsHeaders">
+        <h2>Nuestros Productos</h2>
+        <p>
+          Somos orgullosos distribuidores de distintas de marcas proveedoras de
+          insumos para tu sistema de tratamiento de agua, contamos con filtros,
+          quimicos, solventes, equipo de bombeo y mas...
+        </p>
+      </div>
+
+      <div className="productsGroup">
+        <ProductCard />
+      </div>
+    </section>
+  );
 }
