0a4d6c8b9d368043b4d9ce211211eff339d2e002
Julio Santacruz <julio.santacruzh@gmail.com>
2023-10-25 17:39:37 -0700

    scrooll touch test

--- src/sections/Projects/index.tsx
+++ src/sections/Projects/index.tsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import './Projects.scss'
-import CardProject from '@/components/CardProject'
+"use client";
+import React from "react";
+import "./Projects.scss";
+import CardProject from "@/components/CardProject";
+import { Slide, Slideshow } from "@/components/Slideshow";
 
 export default function Projects() {
   return (
-    <section className='projectsIqea'>
+    <section className="projectsIqea">
       <div className="headerProjects">
-        <h2>
-        PROYECTOS IQEA
-        </h2>
+        <h2>PROYECTOS IQEA</h2>
         <p>
-          Construimos sistemas de tratamiento de agua "llave en mano", elaboramos proyectos, y trabajamos en equipo. 
+          Construimos sistemas de tratamiento de agua "llave en mano",
+          elaboramos proyectos, y trabajamos en equipo.
         </p>
       </div>
 
       <div className="bodyProjects ">
-        <CardProject />
+        <Slideshow autoplay={true} controls={false} styleProps={{ width: "380px", overflowX:'scroll', scrollbarWidth:'none' }}>
+          <Slide>
+            <CardProject />
+          </Slide>{" "}
+          <Slide>
+            <CardProject />
+          </Slide>{" "}
+          <Slide>
+            <CardProject />
+          </Slide>{" "}
+          <Slide>
+            <CardProject />
+          </Slide>{" "}
+          <Slide>
+            <CardProject />
+          </Slide>{" "}
+          <Slide>
+            <CardProject />
+          </Slide>
+        </Slideshow>
       </div>
-
-
     </section>
-  )
+  );
 }
- 
