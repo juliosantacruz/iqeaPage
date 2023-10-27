5f469c48b7587177fef86b98cd3c3b34808397fd
Julio Santacruz <julio.santacruzh@gmail.com>
2023-10-26 19:38:36 -0700

    navbar funtion fixed

--- src/layout/Navbar/index.tsx
+++ src/layout/Navbar/index.tsx
@@ -1,24 +1,27 @@
-'use client'
+"use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 import Link from "next/link";
-import {  usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import logoIqea from "@/assets/iqea_logo.png";
 import IconMenu from "@/components/Icons/IconMenu";
 
 export default function Navbar() {
-  const [openMenu, setOpenMenu] = useState(false)
-  const path =  usePathname() 
+  const [openMenu, setOpenMenu] = useState(false);
+  const path = usePathname();
 
-const isActiveLink = (value:string) =>{
-  return `navLink ${value===path?'active':''}`
-}
-const handlerMenu=()=>{
-  setOpenMenu(!openMenu)
-  console.log(openMenu)
-}
+  const isActiveLink = (value: string) => {
+    return `navLink ${value === path ? "active" : ""}`;
+  };
+  useEffect(()=>{
+    setOpenMenu(false)
+  },[path])
+  const handlerMenu = () => {
+    setOpenMenu(!openMenu);
+    console.log(openMenu);
+  };
 
   return (
     <header>
@@ -28,38 +31,40 @@ const handlerMenu=()=>{
         </div>
         <div className="movilMenu">
           <button onClick={handlerMenu} className="buttonMenu">
-            <IconMenu className="hamburger"/>
-
+            <IconMenu className="hamburger" />
           </button>
         </div>
-        <ul className={`navLinks ${openMenu?'openMenu':''}`}>
+        <ul className={`navLinks ${openMenu ? "openMenu" : ""}`}>
           <li>
-            <Link href="/" className={isActiveLink('/')}>
+            <Link href="/" className={isActiveLink("/")}>
               Inicio
             </Link>
           </li>
           <li>
-            <Link href='/quienes-somos' className={isActiveLink('/quienes-somos')}>
+            <Link
+              href="/quienes-somos"
+              className={isActiveLink("/quienes-somos")}
+            >
               Quiénes Somos
             </Link>
           </li>
           <li>
-            <Link href="/servicios" className={isActiveLink('/servicios')}>
+            <Link href="/servicios" className={isActiveLink("/servicios")}>
               Servicios
             </Link>
           </li>
           <li>
-            <Link href='/productos' className={isActiveLink('/productos')}>
+            <Link href="/productos" className={isActiveLink("/productos")}>
               Productos
             </Link>
           </li>
           <li>
-            <Link href="/proyectos" className={isActiveLink('/proyectos')}>
+            <Link href="/proyectos" className={isActiveLink("/proyectos")}>
               Proyectos
             </Link>
           </li>
           <li>
-            <Link href="/contacto" className={isActiveLink('/contacto')}>
+            <Link href="/contacto" className={isActiveLink("/contacto")}>
               Contacto
             </Link>
           </li>
