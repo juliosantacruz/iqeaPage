ea0594e746c8d0dead251e7284d394ee85410fda
Julio Santacruz <julio.santacruzh@gmail.com>
2023-11-10 22:29:33 -0800

    test submit fomulario

--- src/components/JsonForm/JSONForm.tsx
+++ src/components/JsonForm/JSONForm.tsx
@@ -1,19 +1,51 @@
 "use client";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import validator from "@rjsf/validator-ajv8";
 import Form from "@rjsf/core";
 import "./JSONForm.scss";
 
 export default function JSONForm({ jsonData, JsonUI }: any) {
-    const log = (type: string) => console.log.bind(console, type);
+    const [formData, setFormData] = useState(null);
+    const handlerOnSubmit = () => {
+        console.log(formData);
+
+        
+        let data = JSON.stringify({
+            data: {
+                formulario: {
+                    formData
+                },
+            },
+        });
+
+        let config = {
+            method: "post",
+            maxBodyLength: Infinity,
+            url: `${process.env.API_URL_STRAPI}/formularios`,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            data: data,
+        };
+
+        axios
+            .request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     return (
         <Form
             schema={jsonData}
             validator={validator}
             uiSchema={JsonUI}
-            // onChange={log("changed")}
-            onSubmit={log("submitted")}
+            onChange={(e) => setFormData(e.formData)}
+            onSubmit={handlerOnSubmit}
         // onError={log("errors")}
         />
     );
