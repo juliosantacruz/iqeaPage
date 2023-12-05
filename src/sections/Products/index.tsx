e59b4781a9aef1b1a08dcf121cbff1f2a6d01c1f
Julio Santacruz <julio.santacruzh@gmail.com>
2023-12-05 13:01:05 -0800

    test modo oscuro

--- src/sections/Products/index.tsx
+++ src/sections/Products/index.tsx
@@ -5,20 +5,16 @@ import { getCatProductos } from "@/services/fetchData";
 import SlideProducts from "@/components/SlideProducts/SlideProducts";
 import Link from "next/link";
 
-
-  const random_array=(arr:any)=>{
-
-    const copiaArray = arr.slice(); // Crear una copia del array para no modificar el original
-    for (let i = copiaArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [copiaArray[i], copiaArray[j]] = [copiaArray[j], copiaArray[i]];
-    }
-
-    const primerosCincoObjetos = copiaArray.slice(0, 5);
-    return primerosCincoObjetos
+const random_array = (arr: any) => {
+  const copiaArray = arr.slice(); // Crear una copia del array para no modificar el original
+  for (let i = copiaArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copiaArray[i], copiaArray[j]] = [copiaArray[j], copiaArray[i]];
   }
 
-
+  const primerosCincoObjetos = copiaArray.slice(0, 5);
+  return primerosCincoObjetos;
+};
 
 export default async function Products() {
   const data = await getCatProductos();
@@ -40,30 +36,32 @@ export default async function Products() {
     return newProdArr;
   });
 
-
-
-  const productData =random_array(category_productArray)
-
+  const productData = random_array(category_productArray);
 
   return (
     <section className="productsIqea">
-      <div className="productsHeaders">
-        <h2>Nuestros Productos</h2>
-        <p>
-          Somos orgullosos distribuidores de distintas de marcas proveedoras de
-          insumos para tu sistema de tratamiento de agua, contamos con filtros,
-          quimicos, solventes, equipo de bombeo y mas...
-        </p>
+      <div className="productsAccent">
+        <div className="productsHeaders">
+          <h2>Nuestros Productos</h2>
+          <p>
+            Somos orgullosos distribuidores de distintas de marcas proveedoras
+            de insumos para tu sistema de tratamiento de agua, contamos con
+            filtros, quimicos, solventes, equipo de bombeo y mas...
+          </p>
+        </div>
+
+        <div className="productsGroup">
+          <SlideProducts productData={productData} />
+        </div>
+        <ButtonStyle>
+          <Link
+            href={"/productos"}
+            style={{ margin: "10px 0", width: "200px" }}
+          >
+            Ver mas...
+          </Link>
+        </ButtonStyle>
       </div>
-
-      <div className="productsGroup">
-
-      <SlideProducts productData={productData}/>
-
-      </div>
-      <ButtonStyle>
-        <Link href={'/productos'} style={{ margin: "30px 0", width: "300px" }}>Ver mas...</Link>
-      </ButtonStyle>
     </section>
   );
 }
--- src/sections/Projects/index.tsx
+++ src/sections/Projects/index.tsx
@@ -14,78 +14,80 @@ export default function Projects({ projectsData }: any) {
   // console.log(projectsData)
   return (
     <section className="projectsIqea">
-      <div className="headerProjects centerDiv">
-        <h2>PROYECTOS IQEA</h2>
-        <p>
-          Minim deserunt occaecat veniam anim eiusmod eu cupidatat anim eiusmod
-          excepteur. Quis proident fugiat enim incididunt ut quis velit sint non
-          non veniam eiusmod reprehenderit. Aliqua ipsum incididunt aliquip
-          laboris sint deserunt elit tempor officia ex elit. Ullamco sit est in
-          elit aliquip sit veniam quis veniam.
-        </p>
+      <div className="projectsAccent">
+        <div className="headerProjects centerDiv">
+          <h2>PROYECTOS IQEA</h2>
+          <p>
+            Minim deserunt occaecat veniam anim eiusmod eu cupidatat anim
+            eiusmod excepteur. Quis proident fugiat enim incididunt ut quis
+            velit sint non non veniam eiusmod reprehenderit. Aliqua ipsum
+            incididunt aliquip laboris sint deserunt elit tempor officia ex
+            elit. Ullamco sit est in elit aliquip sit veniam quis veniam.
+          </p>
+        </div>
+
+        <div className="bodyProjects ">
+          <Swiper
+            breakpoints={{
+              // when window width is >= 640px
+              320: {
+                width: 320,
+                slidesPerView: 1.1,
+              },
+              // when window width is >= 640px
+              640: {
+                width: 640,
+                slidesPerView: 1.6,
+              },
+              // when window width is >= 768px
+              768: {
+                width: 768,
+                slidesPerView: 2,
+              },
+              // when window width is >= 1240px
+              1240: {
+                width: 1240,
+                slidesPerView: 3.3,
+              },
+            }}
+            // modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={10}
+            // onSlideChange={() => console.log("slide change")}
+            // onSwiper={(swiper) => console.log(swiper)}
+          >
+            {projectsData?.map((image: any) => {
+              const [error, setError] = useState(false);
+              const handleImageError = (e: any) => {
+                console.error(`Error loading image: ${e}`);
+                setError(true);
+              };
+
+              const setUrl = (src: string, error: boolean) => {
+                if (!error) {
+                  return src;
+                }
+                if (error) {
+                  return ImageNotFound;
+                }
+              };
+
+              return (
+                <SwiperSlide key={image.id}>
+                  <CardProject leProject={image} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+        <ButtonStyle>
+          <Link
+            href={"/proyectos"}
+            style={{ maxWidth: "200px", margin: "30px 0" }}
+          >
+            Ver mas...
+          </Link>
+        </ButtonStyle>
       </div>
-
-      <div className="bodyProjects ">
-        <Swiper
-          breakpoints={{
-            // when window width is >= 640px
-            320: {
-              width: 320,
-              slidesPerView: 1.1,
-            },
-            // when window width is >= 640px
-            640: {
-              width: 640,
-              slidesPerView: 1.6,
-            },
-            // when window width is >= 768px
-            768: {
-              width: 768,
-              slidesPerView: 2,
-            },
-            // when window width is >= 1240px
-            1240: {
-              width: 1240,
-              slidesPerView: 3.3,
-            },
-          }}
-          // modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={10}
-          // onSlideChange={() => console.log("slide change")}
-          // onSwiper={(swiper) => console.log(swiper)}
-        >
-          {projectsData?.map((image: any) => {
-            const [error, setError] = useState(false);
-            const handleImageError = (e: any) => {
-              console.error(`Error loading image: ${e}`);
-              setError(true);
-            };
-
-            const setUrl = (src: string, error: boolean) => {
-              if (!error) {
-                return src;
-              }
-              if (error) {
-                return ImageNotFound;
-              }
-            };
-
-            return (
-              <SwiperSlide key={image.id}>
-                <CardProject leProject={image} />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </div>
-      <ButtonStyle>
-        <Link
-          href={"/proyectos"}
-          style={{ maxWidth: "350px", margin: "30px 0" }}
-        >
-          Ver mas...
-        </Link>
-      </ButtonStyle>
     </section>
   );
 }
