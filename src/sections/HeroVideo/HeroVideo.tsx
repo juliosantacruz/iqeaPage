7738469080a67c1dcbbe2ac9bfda36de1d52ecee
Julio Santacruz <julio.santacruzh@gmail.com>
2024-01-04 11:58:48 -0800

    ajuste resposive hero

--- src/sections/HeroVideo/HeroVideo.tsx
+++ src/sections/HeroVideo/HeroVideo.tsx
@@ -10,23 +10,10 @@ export default function HeroVideo() {
   return (
     <section id="parent-container" className="HeroVideo" style={{zIndex:'100'}}>
       <video src={video} autoPlay muted loop className="videoBG" />
-
-      {/* <div className="title">
-        <h1>
-          Experiencia <span className="text-gradient">IQEA</span>
-        </h1>
-        <p>
-          {" "}
-          Limpiamos el planeta, tenemos experiencia diseniando y construyendo
-          sistemas de tratamiento de agua y procesos, resolviendo los retos mas
-          complejos, contamos con un portafolio de soliciones concretas para tu
-          proyecto
-        </p>
-      </div> */}
       <div className="heroData" >
         <div className="title">
           <Image src={iqeaLogo as any} alt="logo" className={"imgHero"} />
-          <h1>Ingenieria</h1>
+          <h2>Ingenieria</h2>
         </div>
           <p>
             Limpiamos el planeta, tenemos experiencia diseñando y construyendo
